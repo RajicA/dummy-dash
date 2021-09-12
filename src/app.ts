b4398691b45e046440b25539eb0c0f6734789c09
Aleksandar Rajic <rajic1133@gmail.com>
2021-09-12 14:16:23 +0200

    Added basic layout

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,6 @@
 import './styles.scss';
-import Data from './assets/data.json';
 
 export class App {
-    private message: string;
-
-    constructor(msg: string) {
-      this.message = msg;
-    }
-
-    public printHello(id: string) {
-      const container = document.getElementById(id);
-      if (!!container) {
-        container.innerHTML = this.message;
-        const a = 'aaaa';
-      } else {
-        console.error(`<element id="${id}" ...> does not exist !`);
-      }
-    }
+  constructor() { }
 }
 
-const appInstance = new App('Welcome to Dummy Dashboard');
-appInstance.printHello('app');
-console.log(Data);
